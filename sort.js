398eb663af33bc8aae7ad52b79d17ffc957f8abc
SummerW <124275346+DaveW3202@users.noreply.github.com>
2023-03-12 21:24:37 +0700

    yes yes yes

---
+++ sort.js
@@ -0,0 +1,9 @@
+const numbers = [5, 2, 8, 1, 4];
+
+// Sort the array in ascending order
+numbers.sort((a, b) => a - b);
+console.log(numbers); // Output: [1, 2, 4, 5, 8]
+
+// Sort the array in descending order
+numbers.sort((a, b) => b - a);
+console.log(numbers); // Output: [8, 5, 4, 2, 1]
