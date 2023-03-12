a9c221ecc5963ca8c435a3be783df43d60f426cb
SummerW <124275346+DaveW3202@users.noreply.github.com>
2023-03-12 21:18:28 +0700

    Update console.js

--- console.js
+++ console.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+// This function takes an array of numbers and returns the sum of their squares
+function sumOfSquares(numbers) {
+  // Use the reduce method to calculate the sum of the squares
+  const sum = numbers.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue ** 2;
+  }, 0);
+  
+  return sum;
+}
+
+// Example usage:
+const numbers = [1, 2, 3, 4, 5];
+const result = sumOfSquares(numbers);
+console.log(result); // Output: 55
